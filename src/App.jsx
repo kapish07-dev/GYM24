3ed575c739c3743a7ec0b2a038c1455a4d6a71cf
kapish07-dev <kapishbarhate7@gmail.com>
2025-11-28 00:07:26 +0530

    audit again

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import './App.css'
-import Hero from './components/Hero/Hero'
-import Reasons from './components/Reasons'
-import Membership from './components/Membership'
-import AboutUs from './components/AboutUs'
-import Copyright from './components/Copyright'
-import Footer from './components/Footer'
-import Trainers from './components/Trainers'
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Reasons from "./components/Reasons";
+import Membership from "./components/Membership";
+import AboutUs from "./components/AboutUs";
+import Copyright from "./components/Copyright";
+import Footer from "./components/Footer";
+import Trainers from "./components/Trainers";
 
 function App() {
-
   return (
     <>
       <Hero />
       <Reasons />
       <Membership />
       <AboutUs />
-      <Trainers/>
+      <Trainers />
       <Footer />
       <Copyright />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
